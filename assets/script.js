8d36a72d22190ca03b6239f763aae89cb0e8bb1f
Shao Ge <geshao3606@gmail.com>
2021-03-15 13:30:36 -0700

    forecast update

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,17 @@ var now = document.querySelector('#weatherNow')
 
 function getWeather(event) {
     event.preventDefault();
+    var forecastlist = document.querySelector('#Forecast');
+    forecastlist.textContent = '';
     weathertoday();
+    getforecast();
 }
 
 function weathertoday(){
     var api_key = 'be617f0a2a928440246df40a1f51db8c';
     var inputcity = cities.value;
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputcity +'&units=imperial&appid=' + api_key;
-    now.classList.add("list-group")
+    now.setAttribute("class","list-group");
 
     fetch(url)
         .then(function (response) {
@@ -41,23 +44,67 @@ function weathertoday(){
             .then(function (data) {
                 var getuv = document.querySelector('#uv');
                 getuv.textContent = 'UV Index: '+ data.value;
+            });
         });
-    });
+}
+
+function getforecast(){
+    var api_key = 'be617f0a2a928440246df40a1f51db8c';
+    var inputcity = cities.value;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputcity +'&units=imperial&appid=' + api_key;
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var lon_city = data.coord.lon;
+            var lat_city = data.coord.lat;
+
+            var forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +lat_city+ '&lon=' +lon_city+ '&units=imperial&exclude=minutely,hourly&appid=' + api_key
+            fetch(forecast_url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var forecastlist = document.querySelector('#Forecast');
+                var titleEl = document.querySelector('h3');
+                titleEl.textContent = '5-Day Forecast';
+                for (let i = 0; i < 5; i++) {
+                    var forecast5day = document.createElement('div');
+                    var listul = document.createElement('ul');
+                    var li_date = document.createElement('li');
+                    var li_temp = document.createElement('li');
+                    var li_humidity = document.createElement('li');
+
+                    forecast5day.setAttribute("id","day");
+                    listul.setAttribute("id","forecastday");
+
+                    li_date.textContent = unixtodate(data.daily[i+1].dt);
+                    li_temp.textContent = 'Temp: '+ data.daily[i+1].temp.day;
+                    li_humidity.textContent = 'Humidity: '+ data.daily[i+1].humidity;
+                    
+                    forecastlist.appendChild(forecast5day);
+                    forecast5day.appendChild(listul);
+                    listul.appendChild(li_date);
+                    listul.appendChild(li_temp);
+                    listul.appendChild(li_humidity);
+                }
+
+            });
+
+        });
 }
 
 function unixtodate(unix){
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
     var findDate = new Date(unix * 1000);
     var year = findDate.getFullYear();
     var date = findDate.getDate();
     var month = months[findDate.getMonth()];
-    var unixtoDate = date + '-' + month + '-' + year;
+    var unixtoDate = month  + '/' + date + '/' + year;
     return unixtoDate;
 }
 
-console.log(unixtodate(1615924800))
-// function forecastWeather() {
-//     api.openweathermap.org/data/2.5/forecast/daily?q=seattle&cnt=5&appid=be617f0a2a928440246df40a1f51db8c
-// }
 
 search.addEventListener('click',getWeather);
